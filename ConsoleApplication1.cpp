5aab0c476d6d3d981b43862da6ed9f1a8b96e6cc
Luis Garcia <luisga0112@icloud.com>
2024-05-17 02:36:59 -0600

    Creo que ya quedo, solo quiero ver si puedo hacer que de un mensaje si el usuario no desea continuar.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -8,22 +8,42 @@ int numPrimo(int);
 
 int main() {
     int min, max;
-    printf("Introduce el inicio del rango: ");
-    scanf_s("%d", &min);
-    printf("Introduce el fin del rango: ");
-    scanf_s("%d", &max);
+    char op;
+    do {
 
-    printf("Números primos en el rango [%d, %d]: ", min, max);
-    for (int i = min; i <= max; ++i) {
 
-    }
+        printf("Introduce el inicio del rango:\n");
+        scanf_s("%d", &min);
+        printf("Introduce el fin del rango:\n");
+        scanf_s("%d", &max);
 
+        printf("Los numeros primos entre %d y %d son: ", min, max);
+        for (int i = min; i <= max; ++i) {
+            if (numPrimo(i)) {
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+
+        printf("\nDeseas continuar? (c=Continuar)\n");
+        scanf_s(" %c", &op);
+        printf("\n");
+    } while (op == 'c' || op == 'C');
+    
     return 0;
-}
+} 
+
+
 
 int numPrimo(int numero) {
     if (numero <= 1) {     //Aqui es donde negamos los negativos, los numeros no pueden ser 1 ni algo menor a este.
         return(0);
     }
-    for(int i=2;i*i<=numero; ++i)
-}
+    for (int i = 2; i*i <= numero; ++i) {  //El bucle continuará mientras el cuadrado de i sea menor o igual al valor de numero
+        if (numero % i == 0) {
+            return 0;      //Si es divisible nos devuelve un false.
+        }
+    }
+    return 1;             //Pero si no, nos devulve un true, diciendonos que si es un numero primo.
+    }
+
