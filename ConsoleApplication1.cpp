18785315d502a6c0ff61f3906fb931b23caaf41f
Luis Garcia <luisga0112@icloud.com>
2024-05-17 00:39:06 -0600

    Inicializando el repositorio

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,10 +1,23 @@
-// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// Realizar un programa que imprima los numeros primos en un rango dado. No se permite el ingreso de numeros negativos.
 
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
-{
-    std::cout << "Hello World!\n";
-}
+
+    {
+        int n, i, x;
+        printf("Introduce Limite Superior\n");
+        scanf_s("%d", &n);
+        printf("Introduce Limite Inferior\n");
+        scanf_s("%d", &x);
+        for (i = x; i <= n; i++)
+        {
+            printf("\n%d", i);
+        }
+        return 0;
+    }
+
+
 
